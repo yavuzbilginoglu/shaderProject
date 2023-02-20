b10e1cd2edb36f59b217324ae1e7e1d8028c8075
yavuzbilginoglu <yavuzbilginoglu@gmail.com>
2023-02-20 14:14:44 +0300

    cameraSwitch

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,10 +11,15 @@ public class CameraController : MonoBehaviour
     private bool cameraStarted;
     public TextMeshProUGUI resolutionText;
     public TextMeshProUGUI fpsText;
+    private string resolutions;
 
+    private void Update()
+    {
+        fpsText.text = "FPS: " + (int)(1f / Time.deltaTime);
+    }
     public void StartCamera()
     {
-        // Eger kamera zaten baslamissa hicbir þey yapma
+        // Eger kamera zaten baslamissa hicbir sey yapma
         if (cameraStarted)
             return;
 
@@ -23,29 +28,61 @@ public class CameraController : MonoBehaviour
         WebCamDevice[] devices = WebCamTexture.devices;
         if (devices.Length == 0)
         {
-            Debug.Log("Kamera bulunamadý");
+            resolutions = "Kamera bulunamadi";
+            return;
+        }
+        //secilen kamerayi tutacak degisken
+        WebCamDevice selectedDevice = devices[0];
+
+        int minDistance = int.MaxValue;
+        //tum kameralarý dondurme
+        for (int i = 0; i < devices.Length; i++)
+        {
+            WebCamDevice device = devices[i];
+            for (int j = 0; j < device.availableResolutions.Length; j++)
+            {
+                //kameralarin destekledigi cozunurlukleri Vector2Int'e donustur
+                Vector2Int resolution = new Vector2Int(device.availableResolutions[j].width, device.availableResolutions[j].height);
+
+                //640x480 ile olan mesafeyi hesapla
+                int distance = (resolution - new Vector2Int(640, 480)).sqrMagnitude;
+                //mevcut kameranin 640x480'e olan mesafesi en kucuk olani kaydet
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    selectedDevice = device;
+                }
+            }
+        }
+
+
+        int selectedResolutionIndex = 0;
+        WebCamTexture webcamTexture = null;
+
+
+        //Secilen kameranin 640x480 cozunurlugune sahip olup olmadigini kontrol et
+        for (int i = 0; i < selectedDevice.availableResolutions.Length; i++)
+        {
+            if (selectedDevice.availableResolutions[i].width == 640 && selectedDevice.availableResolutions[i].height == 480)
+            {
+                selectedResolutionIndex = i;
+                webcamTexture = new WebCamTexture(selectedDevice.name, selectedDevice.availableResolutions[selectedResolutionIndex].width, selectedDevice.availableResolutions[selectedResolutionIndex].height);
+                break;
+            }
+        }
+        //secilen kameranin 640x480 cozunurlugu yoksa en yakin cozunurlugu kullan
+        if (webcamTexture == null)
+        {
+            selectedResolutionIndex = 0;
+            webcamTexture = new WebCamTexture(selectedDevice.name, selectedDevice.availableResolutions[selectedResolutionIndex].width, selectedDevice.availableResolutions[selectedResolutionIndex].height);
+        }
+
+        if (selectedResolutionIndex == -1)
+        {
+            resolutions = "640x480 res destekleyen kamera bulunamadi.";
             return;
         }
         
-        //640x480 e en yakin resolusyonu alma
-        int closestIndex = 0;
-        //kameranýn destekledigi resolusyonlarýný tek tek aliyoruz
-        for (int i = 1; i < devices[0].availableResolutions.Length; i++)
-        {
-            //abs ile mutlak deger aliyoruz
-            //boylece 640 a veya 480 e en yakýn degeri bulmus oluyoruz
-            int diff1 = Mathf.Abs(devices[0].availableResolutions[i].width - 640);
-            int diff2 = Mathf.Abs(devices[0].availableResolutions[closestIndex].width - 640);
-            int diff3 = Mathf.Abs(devices[0].availableResolutions[i].height - 480);
-            int diff4 = Mathf.Abs(devices[0].availableResolutions[closestIndex].height - 480);
-            //en yakin degeri bulma 
-            if (diff1 + diff3 < diff2 + diff4)
-            {
-                closestIndex = i;
-            }
-        }
-        //kamera nesnesini gelen degerlere gore alma
-        webcamTexture = new WebCamTexture(devices[0].name, devices[0].availableResolutions[closestIndex].width, devices[0].availableResolutions[closestIndex].height);
         //UI elemanina atama
         display.texture = webcamTexture;
         
@@ -53,12 +90,9 @@ public class CameraController : MonoBehaviour
         webcamTexture.Play();
 
         
-        string resolutions = "Current Resolution: " + webcamTexture.width + "x" + webcamTexture.height;
+        resolutions = "Current Resolution: " + webcamTexture.width + "x" + webcamTexture.height;
         resolutionText.text = resolutions;
 
     }
-    private void Update()
-    {
-        fpsText.text = "FPS: " + (int)(1f / Time.deltaTime);
-    }
+    
 }
