ec75d8d760a35dd2c4f508e64e3631584b392cf1
yavuzbilginoglu <yavuzbilginoglu@gmail.com>
2023-03-29 21:39:23 +0300

    webCamTexture

--- Assets/ComputeShaders/ComputeShaderExample.cs
+++ Assets/ComputeShaders/ComputeShaderExample.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ComputeShaderExample : MonoBehaviour
 {
     public ComputeShader shader;
     public RawImage rawImage;
+    private int width = 640;
+    private int height = 480;
 
+    private WebCamTexture webcamTexture;
     private RenderTexture renderTextureKernel1;
     private RenderTexture renderTextureKernel2;
-    public int width = 512;
-    public int height = 512;
-    public Texture2D sourceTexture;
 
     private void Start()
     {
+        webcamTexture = new WebCamTexture();
+        webcamTexture.Play();
 
         int kernel1 = shader.FindKernel("ChangeGreen");
         int kernel2 = shader.FindKernel("ChangeRed");
@@ -29,20 +33,23 @@ public class ComputeShaderExample : MonoBehaviour
         renderTextureKernel2.enableRandomWrite = true;
         renderTextureKernel2.Create();
 
-        //RenderTexture.active = renderTextureKernel1;
+        StartCoroutine(ProcessWebcamTexture(kernel1, kernel2));
+    }
 
-        //Graphics.Blit(sourceTexture, renderTextureKernel1);
+    IEnumerator ProcessWebcamTexture(int kernel1, int kernel2)
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame(); //her framede tekrar calismasi icin
+            shader.SetTexture(kernel1, "SourceTex", webcamTexture);
+            shader.SetTexture(kernel1, "_ResultTex", renderTextureKernel1);
+            shader.Dispatch(kernel1, width / 8, height / 8, 1);
 
-        shader.SetTexture(kernel1, "SourceTex", sourceTexture);
-        shader.SetTexture(kernel1, "_ResultTex", renderTextureKernel1);
-        shader.Dispatch(kernel1, width / 8, height / 8, 1);
-
-
-        shader.SetTexture(kernel2, "SourceTex", renderTextureKernel1);
-        shader.SetTexture(kernel2, "_ResultTex", renderTextureKernel2);
-        shader.Dispatch(kernel2, width / 8, height / 8, 1);
-        
-        rawImage.texture = renderTextureKernel2;
-
+            shader.SetTexture(kernel2, "SourceTex", renderTextureKernel1);
+            shader.SetTexture(kernel2, "_ResultTex", renderTextureKernel2);
+            shader.Dispatch(kernel2, width / 8, height / 8, 1);
+            
+            rawImage.texture = renderTextureKernel2;
+        }
     }
 }
